5a8414753a3fc8c8800f2777680f2e5504023d58
saransh-dhama <saransh014@gmail.com>
2020-07-26 13:11:51 +0200

    Added git page deployment script and a devpackage

---
+++ build/precache-manifest.2977d2bf0e04265d17b82127ef9634f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67c0fdbb9739c6e1106f31846efb379",
+    "url": "/webGearsTask/index.html"
+  },
+  {
+    "revision": "3d026544f38af6cf9490",
+    "url": "/webGearsTask/static/js/2.982d4dd0.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/webGearsTask/static/js/2.982d4dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca03d6955a3528857dae",
+    "url": "/webGearsTask/static/js/main.1b810b3a.chunk.js"
+  },
+  {
+    "revision": "bf3b20930b2b7ba66fd0",
+    "url": "/webGearsTask/static/js/runtime-main.f4d81896.js"
+  },
+  {
+    "revision": "aa987d5838672b23172d60202f928928",
+    "url": "/webGearsTask/static/media/product.aa987d58.svg"
+  }
+]);
